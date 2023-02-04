szupie/supercontinents

commit a1cf1324a172a93e4e551208ae5ccab9a3658e32

performance improvement: prevent map updates on scroll when no map is displayed

js/map-selector.js
@@ -164,7 +164,9 @@ function setMapToMya(targetMya) {
 		resetClassForAllMaps('selected', currentMapIndex);
 
 		// trigger redraw
-		updateCallback(prevMya, currentMya);	
+		if (!noMap) {
+			updateCallback(prevMya, currentMya);	
+		}
 	}
 }
 

commit d1a7b2aaa478b8d0e999a3e5373407ba6fafe5da

fix no map message bug introduced by a1cf132

js/app.js
@@ -55,15 +55,16 @@ async function handleMyaUpdate(prevMya, newMya) {
 		case mapSelector.MapTypes.TEXTURE:
 			document.body.setAttribute('data-map-type', 'texture');
 			await loadAndUpdateTexture();
+			globeOverlays.handleMyaUpdate(prevMya, newMya);
 			break;
 		case mapSelector.MapTypes.VECTOR:
 			document.body.setAttribute('data-map-type', 'vector');
+			globeOverlays.handleMyaUpdate(prevMya, newMya);
 			break;
 		default:
 			document.body.setAttribute('data-map-type', 'none');
 			break;
 	}
-	globeOverlays.handleMyaUpdate(prevMya, newMya);
 }
 
 // Make an asynchronous request to load the current map texture,

js/map-selector.js
@@ -148,6 +148,7 @@ function setMapToMya(targetMya) {
 		if (noMap) {
 			currentMapType = MapTypes.NONE;
 			currentMya = Math.round(targetMya/100)*100;
+			// update rough time indicator position in timeline
 			document.getElementById('no-map-indicator').style.top = 
 				`${myaToPercent(targetMya)}%`;
 		} else if (currentMapIndex < textureMapDates.length) {
@@ -164,9 +165,7 @@ function setMapToMya(targetMya) {
 		resetClassForAllMaps('selected', currentMapIndex);
 
 		// trigger redraw
-		if (!noMap) {
-			updateCallback(prevMya, currentMya);	
-		}
+		updateCallback(prevMya, currentMya);	
 	}
 }
 